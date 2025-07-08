7ceb58342ab735b2827c89de0b8786d657dac39d
Ajinsoft <Ajinsoft@DESKTOP-NV0AGB2>
2025-07-08 13:02:02 -0300

    Ignorando node_modules e outros arquivos

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Cadastro } from './cadastro/cadastro';
+
+export const routes: Routes = [
+    { path: '', redirectTo: "login", pathMatch: "full" },
+    { path: 'login', component: Login }, 
+    { path: 'cadastro', component: Cadastro}
+];
---
+++ src/app/cadastro/cadastro.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.css'
+})
+export class Cadastro {
+}
