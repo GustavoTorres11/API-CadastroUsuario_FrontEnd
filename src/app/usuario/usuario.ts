90a552c81a638f1c1af238ebfbe7bce1cd3f5128
GustavoTorres11 <gustavootorres16@gmail.com>
2025-08-01 13:15:45 -0300

    2025/08/01 - Alteração de estilização

--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../services/usuario';
 import { UsuarioListar } from '../models/usuario';
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
   templateUrl: './usuario.html',
-  styleUrl: './usuario.css'
+  styleUrls: ['./usuario.css']
 })
-export class Usuario {
-  usuario!: UsuarioListar;
+export class Usuario implements OnInit {
+
+  usuario: UsuarioListar = {} as UsuarioListar;
 
   constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
-    this.usuarioService.GetUsuario().subscribe(response => {
-      console.log('Dados recebidos', response);
-      this.usuario = response;
+    this.usuarioService.GetUsuario().subscribe({
+      next: (response) => {
+        console.log('Dados recebidos:', response);
+        this.usuario = response;
+      },
+      error: (err) => {
+        console.error('Erro ao buscar usuário:', err);
+      }
     });
+  }
+
+  getInitials(name: string | undefined): string {
+    if (!name) return 'U';
+    return name.split(' ').map(n => n[0]).join('').toUpperCase();
   }
 }
